c65b2cf1d095d648f57467f3510a851c366f33a5
lgstuehrmann <lgstuehrmann18@gmail.com>
2018-02-20 12:36:18 -0500

    added score calcs and print statements - still need to do img preprocessing and fully do model

--- kerasnn.py
+++ kerasnn.py
@@ -1,23 +1,27 @@
-import keras as k
+from keras.models import Sequential
+from keras.layers import Dense, Activation
 import numpy as np
-import sklearn as scikit
-
-model = k.Sequential()
-model.add(k.Dense(10, input_shape=(28*28, ), kernel_initializer='he_normal'))
-model.add(k.Activation('relu'))
+#Img Preprocessing
+batch_size = 512
+epochs=10
 
 
+#Begin Model
+model = Sequential()
+model.add(Dense(10, input_shape=(28*28, ), kernel_initializer='he_normal'))
+model.add(Activation('relu'))
 
+model.add(Dense(10, kernel_initializer='he_normal'))
+model.add(Activation('softmax'))
 
-
-
-
-model.add(k.Dense(10, kernel_initializer='he_normal'))
-model.add(k.Activation('softmax'))
-
+# Compile Model
 model.compile(optimizer='sgd', loss='categorical_crossentropy', metrics=['accuracy'])
 
+#Fit model
 history=model.fit(x_train, y_train, validation_data = (x_val, y_val),
-                  epochs = 10, batch_size=512)
-
+                  epochs=epochs, batch_size=batch_size)
+#Get Score
+score=model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy: ', score[1])
 print(history.history)
