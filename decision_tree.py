8dea242e4589d55207ef288ab9980143e9207e2b
bobeart <robertharrison43@gmail.com>
2018-02-26 17:58:07 -0500

    added confusion matrix construction method to tools

--- decision_tree.py
+++ decision_tree.py
@@ -5,10 +5,12 @@ project 2
 """
 import numpy as np
 from tools import preprocess
+from tools import confusion
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
 from matplotlib import pyplot as plt
+
 
 # load image data and labels data
 images_train, images_test, images_valid, labels_train, labels_test, labels_valid = preprocess()
@@ -17,24 +19,26 @@ baseTree = DecisionTreeClassifier(random_state=100)
 # create a modified tree changing different values to get 
 # the confusion matrix to fit better
 modTree = DecisionTreeClassifier(criterion='gini', splitter='best', max_depth=10,
-	min_samples_split=2, min_samples_lead=1, min_weight_fraction_leaf=0.0, max_features=None,
-	random_state=None, max_leaf_nodes=None, min_impurity_decrease=0.0, min_impurity_split=None,
-	class_weight=None, presort=False)
+	min_samples_split=2, min_samples_leaf=1, min_weight_fraction_leaf=0.0, max_features=None,
+	random_state=100, max_leaf_nodes=None, class_weight=None, presort=False)
 
 # train trees using fit
 baseTree.fit(images_train, labels_train)
 modTree.fit(images_train, labels_train)
-# Now run an accuracy test of the tree
-labels_predict = baseTree.predict(images_test)
+# Now create prediction values from the test values run on the tree
+base_predict = baseTree.predict(images_test)
+mod_predict = modTree.predict(images_test)
 
-accuracy_score(labels_test, labels_predict)
-confusion_test1 = [0]*len(labels_test)
-confusion_predict1 = [0]*len(labels_predict)
-n = 0
-
-for n in range(0, len(labels_test)):
-	confusion_test1[n] = np.argmax(labels_test[n])
-	confusion_predict1[n] = np.argmax(labels_predict[n])
-# create a confusion matrix for the test and prediction parts
-cm = confusion_matrix(confusion_test1, confusion_predict1)
-print(cm)
+# create the confusion matrix for the trees
+cm_base = confusion(labels_test, base_predict)
+cm_mod = confusion(labels_test, base_predict)
+print('Base Decision Tree Confusion Matrix')
+print(cm_base)
+print('Modified Decision Tree Confusion Matrix')
+print(cm_mod)
+print('Custom Feature Decision Tree Confusion Matrix')
+"""
+images_test = np.reshape(images_test, (28,28))
+plt.imshow(images_test[1], interpolation='nearest')
+plt.show()
+"""
--- tools.py
+++ tools.py
@@ -4,6 +4,7 @@ python file that contains the function which preprocesses the data
 """
 from keras import utils as np_utils
 import numpy as np
+from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
 def preprocess():
 	# download the images from the .npy files
@@ -23,4 +24,13 @@ def preprocess():
 			images_train, labels_train, test_size = .8, random_state = 42)
 	return images_train, images_test, images_valid, labels_train, labels_test, labels_valid
 
+# streamline the process of making confusion matrices
+def confusion(test, predict):
+	confusion_test = [0]*len(test)
+	confusion_predict = [0]*len(predict)
 
+	for n in range(0, len(test)):
+		confusion_test[n] = np.argmax(test[n])
+		confusion_predict[n] = np.argmax(predict[n])
+	# create a confusion matrix for the test and prediction parts
+	return confusion_matrix(confusion_test, confusion_predict)
