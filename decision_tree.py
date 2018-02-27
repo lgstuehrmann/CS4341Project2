2d4ab403a3fdc0d7f1d532e3a03690da7286a757
bobeart <robertharrison43@gmail.com>
2018-02-27 17:03:12 -0500

    added functions to tools and created a custom features decision tree that is noticebly worse than the other verions

--- decision_tree.py
+++ decision_tree.py
@@ -4,41 +4,91 @@ The Code for the decision tree model and training for
 project 2
 """
 import numpy as np
-from tools import preprocess
-from tools import confusion
+import tools as tools
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
 from matplotlib import pyplot as plt
-
+from keras import utils as np_utils
+target_names = ['0','1','2','3','4','5','6','7','8','9']
 
 # load image data and labels data
-images_train, images_test, images_valid, labels_train, labels_test, labels_valid = preprocess()
-# create a decision tree using DecisionTreeClassifer
-baseTree = DecisionTreeClassifier(random_state=100)
-# create a modified tree changing different values to get 
+images_train, images_test, images_valid, labels_train, labels_test, labels_valid = tools.preprocess()
+
+#create the Custom Features for each of the sets
+cimg_train = tools.CustomFeat(images_train, len(labels_train))
+cimg_valid = tools.CustomFeat(images_valid, len(labels_valid))
+cimg_test = tools.CustomFeat(images_test, len(labels_test))
+
+# Start by Creating the 3 Decision Tree models
+# Create a basic decision tree using DecisionTreeClassifer
+basTree = DecisionTreeClassifier(random_state=100)
+# Create a modified tree changing different values to get 
 # the confusion matrix to fit better
-modTree = DecisionTreeClassifier(criterion='gini', splitter='best', max_depth=10,
-	min_samples_split=2, min_samples_leaf=1, min_weight_fraction_leaf=0.0, max_features=None,
+modTree = DecisionTreeClassifier(criterion='gini', splitter='best', max_depth=28,
+	min_samples_split=10, min_samples_leaf=1, min_weight_fraction_leaf=0.0, max_features=None,
 	random_state=100, max_leaf_nodes=None, class_weight=None, presort=False)
+# Create a Decision Tree based on custom arguements made
+# from the MNIST data
+custTree = DecisionTreeClassifier(random_state=100)
 
 # train trees using fit
-baseTree.fit(images_train, labels_train)
+basTree.fit(images_train, labels_train)
 modTree.fit(images_train, labels_train)
-# Now create prediction values from the test values run on the tree
-base_predict = baseTree.predict(images_test)
-mod_predict = modTree.predict(images_test)
+custTree.fit(cimg_train, labels_train)
 
-# create the confusion matrix for the trees
-cm_base = confusion(labels_test, base_predict)
-cm_mod = confusion(labels_test, base_predict)
-print('Base Decision Tree Confusion Matrix')
-print(cm_base)
-print('Modified Decision Tree Confusion Matrix')
-print(cm_mod)
-print('Custom Feature Decision Tree Confusion Matrix')
-"""
-images_test = np.reshape(images_test, (28,28))
-plt.imshow(images_test[1], interpolation='nearest')
-plt.show()
-"""
+# check accuracy of Decision Trees on validation set
+base_val = basTree.predict(images_valid)
+mod_val = modTree.predict(images_valid)
+cust_val = custTree.predict(cimg_valid)
+
+print('Basic Tree Validation Accuracy')
+print(accuracy_score(labels_valid, base_val))
+print('Modified Tree Validation Accuracy')
+print(accuracy_score(labels_valid, mod_val))
+print('Custom Tree Validation Accuracy')
+print(accuracy_score(labels_valid, cust_val))
+
+# Now create prediction values from the test values run on the tree
+bas_predict = basTree.predict(images_test)
+mod_predict = modTree.predict(images_test)
+cust_predict = custTree.predict(cimg_test)
+
+
+# create the confusion matrix for the base tree
+cm_bas = tools.confusion(labels_test, bas_predict)
+
+print('Classification Report for Basic Tree')
+print('Basic Tree Test Accuracy')
+print(accuracy_score(labels_test, bas_predict))
+print(classification_report(labels_test, bas_predict))
+tools.dispMatrix(cm_bas, 'Base Decision Tree Confusion Matrix')
+
+# Look at visualizations for base decision tree
+print('Visualization of 3 mistakes made in base tree')
+tools.visualize(images_test, labels_test, bas_predict)
+
+# create the confusion matrix for the modified tree
+cm_mod = tools.confusion(labels_test, mod_predict)
+print('Classification Report for Modified Tree')
+print('Modified Tree Test Accuracy')
+print(accuracy_score(labels_test, mod_predict))
+print(classification_report(labels_test, mod_predict))
+tools.dispMatrix(cm_mod, 'Modified Decision Tree Confusion Matrix')
+# Look at the visualizations for the modified decision tree
+print('Visualization of 3 mistakes made in modified tree')
+tools.visualize(images_test, labels_test, mod_predict)
+
+# create the confusion matrix for the Custom Features Tree
+cm_cust = tools.confusion(labels_test, cust_predict)
+print(cm_cust)
+print('Classification Report for Custom Feature Tree')
+print('Custom Feature Tree Test Accuracy')
+print(accuracy_score(labels_test, cust_predict))
+print(classification_report(labels_test, cust_predict))
+tools.dispMatrix(cm_cust, 'Custom Feature Decision Tree Confusion Matrix')
+# Look at the visualizations for the custom Features Tree
+print('Visualization of 3 mistakes made in Custom Features tree')
+tools.visualize(images_test, labels_test, cust_predict)
--- tools.py
+++ tools.py
@@ -2,11 +2,11 @@
 tools.py
 python file that contains the function which preprocesses the data
 """
-from keras import utils as np_utils
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
-
+from keras import utils as np_utils
 
 def preprocess():
     # download the images from the .npy files
@@ -26,7 +26,6 @@ def preprocess():
         images_train, labels_train, test_size=.8, random_state=42)
     return images_train, images_test, images_valid, labels_train, labels_test, labels_valid
 
-
 # streamline the process of making confusion matrices
 def confusion(test, predict):
     confusion_test = [0] * len(test)
@@ -37,3 +36,74 @@ def confusion(test, predict):
         confusion_predict[n] = np.argmax(predict[n])
     # create a confusion matrix for the test and prediction parts
     return confusion_matrix(confusion_test, confusion_predict)
+
+# visualize the first 3 misinterpreted values from the learning algorithm
+def visualize(images_input, labels_input, predict_input):
+	images = np.reshape(images_input, (len(labels_input), 28,28))
+	labels = [0] * len(labels_input)
+	predict = [0] * len(predict_input)
+	for n in range(0, len(labels_input)):
+		labels[n] = np.argmax(labels_input[n])
+		predict[n] = np.argmax(predict_input[n])
+	n = 0
+	i = 0
+	for n in range(0, len(labels)):
+		if labels[n] != predict[n]:
+			plt.imshow(images[n], interpolation='nearest', cmap='binary')
+			print('Actual image value:') 
+			print(labels[n])
+			print('Predicted image value:')
+			print(predict[n])
+			plt.show()
+			i += 1
+		if i == 3:
+			break
+	return 0;
+
+# function the displays a confusion matrix
+def dispMatrix(matrix, title):
+	figure, ax = plt.subplots()
+	plt.ylabel('Predictions')
+	plt.xlabel('Actual')
+	plt.title(title)
+	plt.xticks([0,1,2,3,4,5,6,7,8,9])
+	plt.yticks([0,1,2,3,4,5,6,7,8,9])
+	ax.matshow(matrix, cmap=plt.cm.Blues)
+	x = [0,1,2,3,4,5,6,7,8,9]
+	for i in x:
+		for j in x:
+			c = matrix[j,i]
+			ax.text(i,j,str(c), va='center', ha='center')
+	
+	plt.show()
+	print(matrix)
+
+
+# create hand engineered features including: average pixel intensity, average number of pixels,
+# longest number of pixels in a row, number of empty spaces
+def CustomFeat(images_input, length):
+	# reshape images into a 28-by-28 matrices
+	images = np.reshape(images_input, (length, 28,28))
+	images_custom = []
+	for i in images:
+		avgInten = 0			# average pixel intensity
+		avgPix = 0				# average number of pixels in the image
+		pixInRow = 0			# longest streak of pixels in a row
+		inARow = 0				# used to help keep track of pixels in a row
+		empty = 0				# keep track of # of empty spaces
+		for x in i:
+			for y in x:
+				#for y in x:
+				avgInten += y
+				if y > 0:
+					avgPix += 1
+					inARow += 1
+				else:
+					inARow = 0
+					empty += 1
+				if inARow > pixInRow:
+					pixInRow = inARow
+		avgInten = avgInten/784
+		avgPix = avgPix/784
+		images_custom.append([avgInten, avgPix, pixInRow, empty])
+	return images_custom
