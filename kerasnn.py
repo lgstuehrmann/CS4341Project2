c6bac2bdf90672a427d76751bdf7b2acddb7cd1d
lgstuehrmann <lgstuehrmann18@gmail.com>
2018-02-26 20:08:50 -0500

    should be final visualization stuff. Since loading a pretrained weight file wasn't working I just put everything into kerasnn.py

--- kerasnn.py
+++ kerasnn.py
@@ -48,5 +48,23 @@ plot.xlabel('Epoch')
 plot.legend(['Train', 'Test'], loc='upper right')
 plot.show()
 
-model.save('knn2000.h5')
+predictions = model.predict(x_test, verbose=0)
+matrix = confusion(y_test, predictions)
+
+figure, ax = plot.subplots()
+plot.ylabel('Predictions')
+plot.xlabel('Actual')
+plot.title('Confusion Matrix for Keras Neural Net')
+plot.xticks([0,1,2,3,4,5,6,7,8,9])
+plot.yticks([0,1,2,3,4,5,6,7,8,9])
+
+ax.matshow(matrix, cmap=plot.cm.Spectral)
+x = [0,1,2,3,4,5,6,7,8,9]
+for i in x:
+    for j in x:
+        c = matrix[j,i]
+        ax.text(i,j,str(c), va='center', ha='center')
+
+plot.show()
+print(matrix)
 
